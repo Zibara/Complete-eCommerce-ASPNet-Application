0350c0e08b4ebcebbd3bdf51cf99709e96181ae1
Ali Zibara <eng.zibara@gmail.com>
2022-06-17 17:27:47 +0300

    2nd Push

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -12,7 +13,7 @@ IWebHostEnvironment environment = builder.Environment;
 builder.Services.AddControllersWithViews();
 
 // DbContext Configuration
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 
 //IConfiguration configuration = app.Configuration;
